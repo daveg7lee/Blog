f719b42dff069a4d0f5853e89d52437c18992d34
daveg7lee <daveg7lee@gmail.com>
2023-05-02 11:50:00 +0900

    ✨ SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://kihyun.me", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Ki hyun lee", // Used in manifest.json
